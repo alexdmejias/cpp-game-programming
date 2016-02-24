5b18b523bd3bdd983464d0e2e217a7c69327e6d7
Alex Mejias <alexdmejias@gmail.com>
2016-02-23 20:34:21 -0500

    init ch2

---
+++ ch2/ch2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ch2
+//
+//  Created by Alex Mejias on 2/23/16.
+//  Copyright © 2016 Alex Mejias. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
